fdc0ef323048f74c02921c6f486930bb5b6b6b77
Tiina Lemettinen <tiina.lemettinen@gmail.com>
2023-10-20 14:32:04 +0300

    unicafe step6

--- osa1/1.6-1.14/src/App.jsx
+++ osa1/1.6-1.14/src/App.jsx
@@ -8,9 +8,10 @@ const Button = ({ text, handleClick }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text}: {value}
-    </p>
+    <tr>
+     <td>{text}</td>
+     <td>{value}</td>
+    </tr>
   )
 }
 
@@ -30,14 +31,16 @@ const App = () => {
       {good + neutral + bad === 0 ? (
         <p>No feedback given</p>
       ) : (
-        <div>
-          <StatisticLine text="Good" value={good} />
-          <StatisticLine text="Neutral" value={neutral} />
-          <StatisticLine text="Bad" value={bad} />
-          <StatisticLine text="All" value={good + neutral + bad} />
-          <StatisticLine text="Average" value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
-          <StatisticLine text="Positive" value={`${((good / (good + neutral + bad)) * 100).toFixed(2)}%`} />
-        </div>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
+            <StatisticLine text="All" value={good + neutral + bad} />
+            <StatisticLine text="Average" value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
+            <StatisticLine text="Positive" value={`${((good / (good + neutral + bad)) * 100).toFixed(2)}%`} />
+          </tbody>
+        </table>
       )}
     </div>
   )
