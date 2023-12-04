0fe4c8e9fe7b1842ca741eebf3137f396bd27fee
Tiina Lemettinen <tiina.lemettinen@gmail.com>
2023-12-04 21:13:13 +0200

    anekdootit

---
+++ osa1/anekdootit/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is the same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast is to go well.'
+  ];
+
+  const initialVotes = Array(anecdotes.length).fill(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({ ...initialVotes });
+
+  const getRandomAnecdot = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const voteForAnecdot = () => {
+    const newVotes = { ...votes };
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const mostVotedAnecdote = () => {
+    let maxVotes = 0;
+    let mostVotedIndex = 0;
+
+    for (let i = 0; i < anecdotes.length; i++) {
+      if (votes[i] > maxVotes) {
+        maxVotes = votes[i];
+        mostVotedIndex = i;
+      }
+    }
+
+    return anecdotes[mostVotedIndex];
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Has {votes[selected]} votes</p>
+      <button onClick={voteForAnecdot}>Vote</button>
+      <button onClick={getRandomAnecdot}>Show Another Anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{mostVotedAnecdote()}</p>
+    </div>
+  );
+};
+
+export default App;
